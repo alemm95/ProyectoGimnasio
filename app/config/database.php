2504df9450024cda55155cbf8f53daa4f2245914
alemstr <alejandromm4495@gmail.com>
2021-04-29 16:40:28 +0200

    v1.0.2

---
+++ app/config/database.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Parámetros de configuración de la base de datos
+ */
+
+define('DBDRIVER', 'mysql');
+define('DBHOST', 'localhost');
+define('DBNAME', 'gimnasio');
+define('DBUSER', 'root');
+define('DBPASS', '');
